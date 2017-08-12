130c0c2b0c0a9111ee8a85ebb732168322327f83
wontaejo87 <wontaejo@yanolja.com>
2017-08-12 11:01:18 +0900

    JdbcTemplate 을 사용한 객체 매핑

--- 2.2.1.1_SpringBoot-JDBC/src/main/java/com/example/App.java
+++ 2.2.1.1_SpringBoot-JDBC/src/main/java/com/example/App.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import com.example.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @EnableAutoConfiguration
 public class App implements CommandLineRunner {
@@ -15,12 +20,15 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT :a + :b";
+        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
         SqlParameterSource param = new MapSqlParameterSource()
-                .addValue("a", 100)
-                .addValue("b", 200);
-
-        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+                .addValue("id", 1);
+        Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+            @Override
+            public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+            }
+        });
 
         System.out.println("result : " + result);
     }
---
+++ 2.2.1.1_SpringBoot-JDBC/src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,14 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    private Integer id;
+    private String first_name;
+    private String last_name;
+}
