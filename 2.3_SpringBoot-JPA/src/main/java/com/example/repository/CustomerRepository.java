c92299d60fe5b124d3307ba9312d7d4d5e5d416d
wontaejo87 <wontaejo@yanolja.com>
2017-08-15 12:44:55 +0900

    JAP는 JPQL 뿐만 아니라 SQL도 사용가능

--- 2.3_SpringBoot-JPA/src/main/java/com/example/repository/CustomerRepository.java
+++ 2.3_SpringBoot-JPA/src/main/java/com/example/repository/CustomerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    @Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
+    //Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
+    @Query(value="SELECT id, first_name, last_name FROM customers ORDER BY first_name, last_name", nativeQuery = true)
     List<Customer> findAllOrderByName();
 }
