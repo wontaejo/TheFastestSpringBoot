56e4b4e32aafce6f948f79a9a624666ffeba1ec3
wontaejo87 <wontaejo@yanolja.com>
2017-08-15 13:01:38 +0900

    2.3.4 페이징 처리 구현하기

--- 2.3_SpringBoot-JPA/src/main/java/com/example/repository/CustomerRepository.java
+++ 2.3_SpringBoot-JPA/src/main/java/com/example/repository/CustomerRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    //Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
-    @Query(value="SELECT id, first_name, last_name FROM customers ORDER BY first_name, last_name", nativeQuery = true)
+    @Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
     List<Customer> findAllOrderByName();
 }
